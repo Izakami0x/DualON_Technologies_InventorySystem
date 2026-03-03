0a19a87d2240a4fa6e93f34d2a48c9e0ee670e3e
Izakmi0x <87597619+Izakami0x@users.noreply.github.com>
2026-03-03 20:42:55 +0800

    Add project files.

---
+++ DualON_Technologies_InventorySystem/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using DualON_Technologies_InventorySystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DualON_Technologies_InventorySystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DualON_Technologies_InventorySystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DualON_Technologies_InventorySystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
