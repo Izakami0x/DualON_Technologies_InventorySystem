30a175a62e41d0671746d8953a450bb59a1bd27f
Izakmi0x <87597619+Izakami0x@users.noreply.github.com>
2026-03-04 19:56:43 +0800

    Apply migration to create database

--- DualON_Technologies_InventorySystem/obj/Debug/net10.0/DualON_Technologies_InventorySystem.AssemblyInfo.cs
+++ DualON_Technologies_InventorySystem/obj/Debug/net10.0/DualON_Technologies_InventorySystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DualON_Technologies_InventorySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ee5db20bfd3b4694f053a145b96972f577b43ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55e1de51ffa9d98baf777cf920fab2eff5939d5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DualON_Technologies_InventorySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DualON_Technologies_InventorySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
