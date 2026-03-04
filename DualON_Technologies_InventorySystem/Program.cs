c3884677e2d33c34d93603bd037b5d6bd2422f88
Izakmi0x <87597619+Izakami0x@users.noreply.github.com>
2026-03-04 20:11:51 +0800

    Register service on Program.cs

--- DualON_Technologies_InventorySystem/Program.cs
+++ DualON_Technologies_InventorySystem/Program.cs
@@ -1,8 +1,11 @@
 using DualON_Technologies_InventorySystem.Data;
 using Microsoft.EntityFrameworkCore;
-
+//Repositories
 using DualON_Technologies_InventorySystem.Repositories;
 using DualON_Technologies_InventorySystem.Repositories.Interfaces;
+//Services
+using DualON_Technologies_InventorySystem.Services;
+using DualON_Technologies_InventorySystem.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args); // <-- this was missing
 
@@ -10,8 +13,13 @@ var builder = WebApplication.CreateBuilder(args); // <-- this was missing
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=inventory.db"));
 
+//Register repositories and services
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+//Registering InventoryService with its interface IInventoryService for dependency injection
+builder.Services.AddScoped<IInventoryService, InventoryService>();
+
+// Add controllers with views (MVC)
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
